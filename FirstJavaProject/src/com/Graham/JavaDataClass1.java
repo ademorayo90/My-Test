f4d9a7e8966f1c0935a376fa8ecc38c4375c8930
ademorayo90 <adeoluwamomo90>
2021-09-16 14:34:43 -0700

    firstJavaProject

---
+++ FirstJavaProject/src/com/Graham/JavaDataClass1.java
@@ -0,0 +1,26 @@
+package com.Graham;
+
+public class JavaDataClass1 {
+    public static void main(String[] args) {
+        byte age = -128;
+        System.out.println(age);
+
+        short sticks = 32_666;
+        System.out.println(sticks);
+
+        long legs = -3456891089l;
+        System.out.println(legs);
+
+        float surface = -2345678.90f;
+
+        System.out.println(surface);
+
+        char bags = 'g';
+        System.out.println('m');
+
+        double Sticks =123456789112.90;
+        System.out.println(Sticks);
+    }
+
+}
+
---
+++ FirstJavaProject/src/com/Graham/Main.java
@@ -0,0 +1,8 @@
+package com.Graham;
+
+public class Main {
+
+    public static void main(String[] args) {
+	 System.out.println("Super excited about this training");
+    }
+}
