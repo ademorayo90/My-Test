936a7bd60dd017d212d24e16f08d4afd4e0edf0c
ademorayo90 <adeoluwamomo90>
2021-09-24 06:46:41 -0700

    ClassA1.java

---
+++ OOPTASK/ClassA1.java
@@ -0,0 +1,10 @@
+package introTOOPP;
+
+public class ClassA1 {
+    String name;
+    public void Set(String myChurchName){
+        name= myChurchName;
+        System.out.println(name);
+
+    }
+}
---
+++ OOPTASK/ClassB2.java
@@ -0,0 +1,9 @@
+package introTOOPP;
+
+public class ClassB2 {
+    public static void main(String[] args) {
+        ClassA1 Street = new ClassA1();
+        Street.name = "harvesters";
+        Street.Set(Street.name);
+    }
+}
