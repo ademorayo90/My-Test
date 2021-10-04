72f31d15c17b8e3629c5013e55dc4800a722ff6f
ademorayo90 <adeniyi.ademorayo90@gmail.com>
2021-10-04 02:19:28 +0100

    lesson43Resize.java

---
+++ lesson43Resize.java
@@ -0,0 +1,19 @@
+import org.checkerframework.checker.units.qual.C;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class lesson43Resize {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\TMD\\IdeaProjects\\Seleniumtwo\\src\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://jqueryui.com/resizable/");
+        driver.switchTo().frame(0);
+        Thread.sleep(5000);
+        WebElement resizeBox = driver.findElement(By.xpath("//div[@id='resizable']"));
+        Actions resize = new Actions(driver);
+        resize.clickAndHold(resizeBox).moveToElement(resizeBox).build().perform();
+    }
+}
